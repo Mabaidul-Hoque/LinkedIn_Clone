0472d91d7bb4f2a1b2663890a38d0c5f92332981
MABAIDUL HOQUE <132288979+Mabaidul-Hoque@users.noreply.github.com>
2024-03-08 21:43:19 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='flex items-center justify-center'>LinkedIn Porject Home page</h1>
+    </div>
+  )
+}
+
+export default App
