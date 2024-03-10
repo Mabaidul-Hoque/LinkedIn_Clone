026f3ef3f6536b1beb32e4a3c16f623b3c278697
MABAIDUL HOQUE <132288979+Mabaidul-Hoque@users.noreply.github.com>
2024-03-10 11:39:33 +0530

    navbar updated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,166 @@
-import React from 'react'
+import {
+  faBell,
+  faBriefcase,
+  faCaretDown,
+  faCircleUser,
+  faCommentDots,
+  faEllipsis,
+  faGripVertical,
+  faHouse,
+  faMagnifyingGlass,
+  faRectangleAd,
+  faUserGroup,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+import AutocompleteModal from "../ui/AutocompleteModal";
 
-const Navbar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+interface NavabrProps {
+  handleMenu: () => void;
 }
+const Navbar: React.FC<NavabrProps> = ({ handleMenu }) => {
+  const [activeIndx, setActiveIndx] = useState(1);
+  const [isSearchClick, setIsSearchClick] = useState(false);
 
-export default Navbar
+  const searchClick = () => {
+    setIsSearchClick((prev) => !prev);
+  };
+
+  return (
+    <div className="w-[95%] xl:w-[82%] m-auto h-full py-2 flex items-center justify-between">
+      {/* LOGO/SEARCH CONTAINER */}
+      <div className="flex items-center gap-4 relative">
+        <img
+          className="w-9 h-9"
+          src="/LinkedIn_icon.svg.png"
+          alt="LinedIn_logo"
+        />
+        {/* SEARCH SECTION */}
+        <div className="flex items-center">
+          <input
+            className={`bg-[#EDF3F8] w-64 xl:focus:w-80 h-9 pl-10 rounded focus:outline-blue-500 ${
+              isSearchClick ? "block" : "hidden"
+            } lg:block`}
+            type="text"
+            id="search-input"
+            placeholder="Search"
+          />
+          {/* SEARCH ICON */}
+          <AutocompleteModal
+            searchClick={searchClick}
+            isSearchClick={isSearchClick}
+          />
+        </div>
+      </div>
+
+      {/* RIGHT ITEMS CONTAINER */}
+      <div className={`flex items-center gap-4`}>
+        <ul className="flex items-center">
+          {/* HOME ROUTE */}
+          <li
+            onClick={() => setActiveIndx(1)}
+            className={`${
+              activeIndx === 1
+                ? "text-black border-b-2 border-black"
+                : "text-gray-600"
+            } hover:text-black p-4 pb-2 pt-2`}
+          >
+            <a className="flex flex-col justify-center items-center" href="#">
+              <FontAwesomeIcon className="w-6 h-6" icon={faHouse} />
+              <span className="text-[12px] max-[800px]:hidden">Home</span>
+            </a>
+          </li>
+          {/* MY NETWORK ROUTE */}
+          <li
+            onClick={() => setActiveIndx(2)}
+            className={`${
+              activeIndx === 2
+                ? "text-black border-b-2 border-black"
+                : "text-gray-600"
+            } hover:text-black p-4 pb-2 pt-2`}
+          >
+            <a className="flex flex-col justify-center items-center" href="#">
+              <FontAwesomeIcon className="w-6 h-6" icon={faUserGroup} />
+              <span className="text-[12px] max-[800px]:hidden">My Network</span>
+            </a>
+          </li>
+          {/* JOBS ROUTE */}
+          <li
+            onClick={() => setActiveIndx(3)}
+            className={`${
+              activeIndx === 3
+                ? "text-black border-b-2 border-black "
+                : "text-gray-600"
+            } hover:text-black p-4 pb-2 pt-2`}
+          >
+            <a className="flex flex-col justify-center items-center" href="#">
+              <FontAwesomeIcon className="w-6 h-6" icon={faBriefcase} />
+              <span className="text-[12px] max-[800px]:hidden">Jobs</span>
+            </a>
+          </li>
+          {/* MESSAGING ROUTE */}
+          <li
+            onClick={() => setActiveIndx(4)}
+            className={`${
+              activeIndx === 4
+                ? "text-black border-b-2 border-black "
+                : "text-gray-600"
+            } hover:text-black p-4 pb-2 pt-2 max-[350px]:hidden`}
+          >
+            <a className="flex flex-col justify-center items-center" href="#">
+              <FontAwesomeIcon className="w-6 h-6" icon={faCommentDots} />
+              <span className="text-[12px] max-[800px]:hidden">Messaging</span>
+            </a>
+          </li>
+          {/* NOTIFICATIONS ROUTE */}
+          <li
+            onClick={() => setActiveIndx(5)}
+            className={`${
+              activeIndx === 5
+                ? "text-black border-b-2 border-black  "
+                : "text-gray-600"
+            } hover:text-black p-4 pb-2 pt-2 max-[410px]:hidden`}
+          >
+            <a className="flex flex-col justify-center items-center" href="#">
+              <FontAwesomeIcon className="w-6 h-6" icon={faBell} />
+              <span className="text-[12px] max-[800px]:hidden">
+                Notifications
+              </span>
+            </a>
+          </li>
+        </ul>
+
+        <div className="flex items-center justify-center gap-4 max-[550px]:hidden">
+          {/* PROFILE SECTION */}
+          <div className="flex flex-col justify-center items-center text-gray-600 hover:text-black cursor-pointer">
+            <FontAwesomeIcon className="w-6 h-6" icon={faCircleUser} />
+            <span className="text-[12px] max-[800px]:hidden">
+              Me <FontAwesomeIcon className="w-3 h-3" icon={faCaretDown} />
+            </span>
+          </div>
+          {/* DIVIDER */}
+          <div className="border-r-1 border-gray-500"></div>
+          {/*  FOR BUSSINESS  */}
+          <div className="flex flex-col justify-center items-center text-gray-600 hover:text-black cursor-pointer">
+            <FontAwesomeIcon className="w-6 h-6" icon={faGripVertical} />
+            <span className="text-[12px] max-[800px]:hidden">
+              For Bussiness{" "}
+              <FontAwesomeIcon className="w-3 h-3" icon={faCaretDown} />
+            </span>
+          </div>
+          {/* ADVERTISE */}
+          <div className="flex flex-col justify-center items-center text-gray-600 hover:text-black cursor-pointer">
+            <FontAwesomeIcon className="w-6 h-6" icon={faRectangleAd} />
+            <span className="text-[12px] max-[800px]:hidden">Advertise</span>
+          </div>
+        </div>
+        {/* RESPONSIVE MENU */}
+        <div onClick={handleMenu} className="cursor-pointer min-[550px]:hidden">
+          <FontAwesomeIcon className="text-3xl" icon={faEllipsis} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/ui/AutocompleteModal.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+
+interface AutocompleteModalProps {
+  searchClick: () => void; // Assuming searchClick is a function that doesn't return anything
+  isSearchClick: boolean; // Assuming isSearchClick is a boolean
+}
+
+const style = {
+  position: "absolute" as "absolute",
+  top: "13%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "green",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+const AutocompleteModal: React.FC<AutocompleteModalProps> = ({
+  searchClick,
+  isSearchClick,
+}) => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  return (
+    <div>
+      <div
+        onClick={() => {
+          searchClick();
+          // handleOpen();
+        }}
+        className={`flex text-gray-500 hover:text-black cursor-pointer ${
+          isSearchClick ? "hidden" : "block"
+        }`}
+      >
+        <FontAwesomeIcon
+          className=" min-[800px]:absolute lg:top-3 min-[800px]:top-0 min-[800px]:left-16"
+          icon={faMagnifyingGlass}
+        />
+        <p className="mt-4 lg:hidden max-[800px]:hidden">Search</p>
+      </div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
+    </div>
+  );
+};
+
+export default AutocompleteModal;
