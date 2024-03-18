23dad5cdd2036f8ca967c14135597173a11f7a01
MABAIDUL HOQUE <132288979+Mabaidul-Hoque@users.noreply.github.com>
2024-03-19 00:05:13 +0530

    unused useeffect removed

--- src/pages/Premium.tsx
+++ src/pages/Premium.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useRef } from "react";
 import { Link } from "react-router-dom";
 import { careerFeatures } from "../data/careerFeatures";
 import { moreFeatures } from "../data/careerFeatures";
